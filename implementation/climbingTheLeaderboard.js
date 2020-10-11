0b612011f15950519b0fd5839d561fdf801bc2f2
darrylgit <darrylsmithup@gmail.com>
2020-10-11 17:40:06 -0400

    Solve Climbing the Leaderboard

--- implementation/climbingTheLeaderboard.js
+++ implementation/climbingTheLeaderboard.js
@@ -1,33 +1,36 @@
 function climbingLeaderboard(scores, alice) {
-  let ranks = []
-  while (alice.length) {
-    let aliceScore = alice.shift();
-  
-    let i = scores.length -1;
-    while (scores[i] < aliceScore) {
-      i --;
-    }
-  
-    // console.log("Scores before: ",scores)
-    scores = [...scores.slice(0, i + 1), aliceScore]
-    // console.log("Scores after: ", scores)
-  
-    const rank = scores.reduce((acc, val, idx, src) => {
-      if (idx === 0) {
-        return acc
-      }
-  
-      return val < src[idx - 1] ? acc + 1 : acc
-    }, 1)
+  let ranks = [];
 
-    ranks.push(rank)
+  // Get distinct scores
+  let scoresSet = [...new Set(scores)];
+
+  while (alice.length) {
+    // Get first element of in alice array
+    let aliceScore = alice.shift();
+
+    let i;
+
+    // Start searching from most recent rank
+    if (ranks.length) {
+      i = ranks[ranks.length - 1] - 2;
+    } else {
+      i = scoresSet.length - 1;
+    }
+
+    // Decrement until aliceScore is less than score in scoreSet
+    while (scoresSet[i] <= aliceScore) {
+      i--;
+    }
+
+    // Insert aliceScore into scoreSet
+    scoresSet.splice(i + 1, aliceScore);
+
+    // Add new rank
+    ranks.push(i + 2);
   }
 
-  
-  return ranks
+  return ranks;
 }
-
-
 
 // RECURSIVE SOLUTION (breaks on large test cases, presumably exceeds stack size)
 // function climbingLeaderboard(scores, alice) {
@@ -51,11 +54,10 @@ function climbingLeaderboard(scores, alice) {
 
 //     return val < src[idx - 1] ? acc + 1 : acc
 //   }, 1)
-  
 
 //   return [rank, ...climbingLeaderboard(scores, alice)]
 // }
 
-console.log(climbingLeaderboard([100, 90, 90, 80, 75, 60], [50, 65, 77, 90, 102]));
-
-
+console.log(
+  climbingLeaderboard([100, 90, 90, 80, 75, 60], [50, 65, 77, 90, 102])
+);
