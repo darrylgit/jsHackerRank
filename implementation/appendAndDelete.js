839a50156af0471e51093305e4ed6ab28e3b6af3
darrylgit <darrylsmithup@gmail.com>
2020-10-14 14:58:43 -0400

    Solve Append and Delete

---
+++ implementation/appendAndDelete.js
@@ -0,0 +1,34 @@
+function appendAndDelete(s, t, k) {
+  // If we can delete s completely and rebuild t from scratch within k operations, return early
+  if (s.length + t.length <= k) {
+    return 'Yes';
+  }
+
+  let operations = 0;
+  let firstDifferentIndex = 0;
+
+  // Find first index that differs between s and t
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] !== t[i]) {
+      break;
+    }
+
+    firstDifferentIndex++;
+  }
+
+  // Increment operations count by number of indices to remove from s, and also by the number of indices we'd add from t
+  operations +=
+    s.length - firstDifferentIndex + (t.length - firstDifferentIndex);
+  // We could simplify this to s.length + t.length - 2 * firstDifferentIndex, but for the sake of legibility...
+
+  // If k is too small, return "No"
+  if (operations > k) {
+    return 'No';
+    // If k is just right, return "Yes"
+  } else if (operations === k) {
+    return 'Yes';
+  }
+
+  // If we have leftover operations, we can only ever arrive at t in an even number of additional operations. Therefore:
+  return (k - operations) % 2 === 0 ? 'Yes' : 'No';
+}
