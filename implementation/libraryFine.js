c11c8ad6c8f83d19637feb90a8ea7021f8b1b6ce
darrylgit <darrylsmithup@gmail.com>
2020-10-16 15:20:14 -0400

    Solve Library Fine

---
+++ implementation/libraryFine.js
@@ -0,0 +1,21 @@
+function libraryFine(d1, m1, y1, d2, m2, y2) {
+  if (y1 < y2) {
+    return 0;
+  }
+
+  if (y1 > y2) {
+    return 10000;
+  }
+
+  if (m1 > m2) {
+    return 500 * (m1 - m2);
+  }
+
+  if (m1 <= m2 && d1 > d2) {
+    return 15 * (d1 - d2);
+  }
+
+  return 0;
+}
+
+console.log(libraryFine(28, 2, 2015, 15, 4, 2015));
